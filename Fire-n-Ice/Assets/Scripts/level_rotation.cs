ac8a60f6e86e743a7ba364c428177642f810b7e0
mrn26 <mrn26@live.com>
2020-03-09 01:31:27 -0600

    it works!
    4 types of obsticals are generated in front of the player. the spawner moves with the player so obsticals are allways generated aways away.
    
    these are the obsticales:
    fire_1
    fire_2
    ice_1
    ice_2
    basically two sizes for each for now
    
    Experiments:
    added a simple encloser to the level and made it rotatable by pressing Q and E. They are different colors so you can see it actually rotating.
    
    Working on getting the obsticals rotating with the level aswell.

---
+++ Fire-n-Ice/Assets/Scripts/level_rotation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class level_rotation : MonoBehaviour
+{
+    void keyPress()
+    {
+        if (Input.GetKeyDown("u"))
+        {
+            transform.position = new Vector3(10,0,0);
+        }
+        if (Input.GetKeyDown("q"))
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x,transform.rotation.eulerAngles.y,transform.rotation.eulerAngles.z + (-90));
+        }
+
+        if (Input.GetKeyDown("e"))
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x,transform.rotation.eulerAngles.y,transform.rotation.eulerAngles.z + (-90));
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        keyPress();
+    }
+}
--- Fire-n-Ice/Assets/Scripts/spawnObsticals.cs
+++ Fire-n-Ice/Assets/Scripts/spawnObsticals.cs
@@ -3,34 +3,87 @@ using System.Collections.Generic;
 using UnityEngine;
 public class spawnObsticals : MonoBehaviour
 {
-    public Obstical obs_1;
-    public Obstical obs_2;
+    public Obstical obs_1;//ice_1
+    public Obstical obs_2;//ice_2
+    public Obstical obs_3;//fire_1
+    public Obstical obs_4;//fire_2
+
+    
+    public GameObject player;
+    public Transform levelRotator;
+
     public GameObject spawner;
     public float spawnTime = 0.2f;
     // Start is called before the first frame update
     void Start()
     {
+        //get our player
+        player = GameObject.Find("FPSController");
         StartCoroutine(wave());
         
-        //look for our player object to get our z point
-        spawner = GameObject.Find("FPSController");
     }
 
     private void spawnObstical()
     {
-        //spawn object
-        Obstical a = Instantiate(obs_1) as Obstical;
+        //randomely pick between fire and ice obsticals
+        int fireOrice = Random.Range(1,4);
 
-        //set its location to our spawner 
-        a.transform.position = new Vector3(Random.Range(-4,4),Random.Range(2,5),spawner.transform.position.z + 20);
+        if(fireOrice == 1)
+        {
+            //spawn ice
+            Obstical newObstical = Instantiate(obs_1) as Obstical;
+            newObstical.transform.position = new Vector3(Random.Range(-4,4),Random.Range(2,5),spawner.transform.position.z);
+
+            //set parent to level rotator NOTE: false means it keeps its local orientation
+            obs_1.transform.SetParent(levelRotator);
+        }
+
+        else if(fireOrice == 2)
+        {
+            //spawn ice_2
+            Obstical newObstical = Instantiate(obs_2) as Obstical;
+            newObstical.transform.position = new Vector3(Random.Range(-4,4),Random.Range(2,5),spawner.transform.position.z);
+
+            //set parent to level rotator NOTE: false means it keeps its local orientation
+            obs_2.transform.SetParent(levelRotator);
+        }
+
+         else if(fireOrice == 3)
+        {
+            //spawn fire_1
+            Obstical newObstical = Instantiate(obs_3) as Obstical;
+            newObstical.transform.position = new Vector3(Random.Range(-4,4),Random.Range(2,5),spawner.transform.position.z);
+
+            //set parent to level rotator NOTE: false means it keeps its local orientation
+            obs_3.transform.SetParent(levelRotator);
+        }
+
+         else if(fireOrice == 4)
+        {
+            //spawn fire_2
+            Obstical newObstical = Instantiate(obs_4) as Obstical;
+             //set its location to our spawner 
+            newObstical.transform.position = new Vector3(Random.Range(-4,4),Random.Range(2,5),spawner.transform.position.z);
+
+            //set parent to level rotator NOTE: false means it keeps its local orientation
+            obs_4.transform.SetParent(levelRotator);
+        }
+    
     }
 
     IEnumerator wave()
     {
+        //while game is running
         while(true)
         {
             yield return new WaitForSeconds(spawnTime);
+
+            //update spawner position
+            spawner.transform.position = new Vector3(transform.position.x,transform.position.y,player.transform.position.z +35);
+
+            //spawn a new obstical
             spawnObstical();
+
         }
     }
 

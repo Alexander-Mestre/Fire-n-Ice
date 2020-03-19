436efb7fd5e0f72dbaad3232fc0699276fdd73a1
Alexander Mestre <44935239+AlexanderMestre@users.noreply.github.com>
2020-03-18 23:07:38 -0600

    Persecuting Wall
    -Added wall
    -Walls moves formard
    -If it touches the player it reload the scene
    -if it touches obstacles it deletes them

---
+++ Fire-n-Ice/Assets/Scripts/Persecution.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Persecution : MonoBehaviour
+{
+
+    public GameObject wall;
+    public float movementSpeed= 10.0f;
+
+    void Update()
+    {
+
+        wall.transform.position += wall.transform.forward * Time.deltaTime * movementSpeed;
+    }
+
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "fire" || collisionInfo.collider.tag == "ice")
+        {
+            
+            Destroy(collisionInfo.gameObject);
+        }
+
+        if (collisionInfo.collider.tag == "Player")
+        {
+            Debug.Log("wall collided with player");
+            SceneManager.LoadScene("Track");
+        }
+    }
+}
